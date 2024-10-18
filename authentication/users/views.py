599d21b01a6883673805773719a9edf9965c02c2
aniceto-jolela <anicetojosejolelaj@gmail.com>
2024-10-18 20:00:25 +0100

    Creation new email field and a new forms file.

--- authentication/users/views.py
+++ authentication/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegister
 
 
 def register(request):
-    form = UserCreationForm()
+    form = UserRegister()
     return render(request, 'users/register.html', {'frm': form})
