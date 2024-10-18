51603f9acc42a443622b759d61e2cdea27dcf528
aniceto-jolela <anicetojosejolelaj@gmail.com>
2024-10-18 20:23:56 +0100

    register user.

--- authentication/users/views.py
+++ authentication/users/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegister
+from django.contrib import messages
 
 
 def register(request):
-    form = UserRegister()
-    return render(request, 'users/register.html', {'frm': form})
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.data.get('username')
+            messages.success(request, f'{user} has been created successful!')
+            return redirect('home')
+    else:
+        form = UserRegister()
+        return render(request, 'users/register.html', {'frm': form})
