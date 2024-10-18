c7d154b999fb1137b03950435ed5e9f08fe184f9
aniceto-jolela <anicetojosejolelaj@gmail.com>
2024-10-18 07:56:31 +0100

    config project (settings and urls)

--- authentication/blog/urls.py
+++ authentication/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('post', views.post, name='post')
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('post/', views.post, name='post')
 ]
