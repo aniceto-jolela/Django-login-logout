941c6990f60a5aac75418f7040f492bbd3c73125
aniceto-jolela <anicetojosejolelaj@gmail.com>
2024-10-22 09:22:45 +0100

    deploy on Render.

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-zi7$^nfm=@8z)&*s*8n)i$lakz0e&@9cs=kn_myoc)3l#ah%b9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
